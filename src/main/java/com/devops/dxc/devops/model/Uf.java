49d955aaf11b66760420892b40c6888045f3314a
Rolando Paredes <rolando2k@gmail.com>
2021-02-03 00:20:30 -0300

    Se sube consulta a api uf

---
+++ src/main/java/com/devops/dxc/devops/model/Uf.java
@@ -0,0 +1,71 @@
+package com.devops.dxc.devops.model;
+
+import java.util.List;
+
+public class Uf {
+
+	private String version;
+	private String autor;
+	private String codigo;
+	private String nombre;
+	private String unidad_medida;
+	private List<Serie> serie;
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getUnidad_medida() {
+		return unidad_medida;
+	}
+
+	public void setUnidad_medida(String unidad_medida) {
+		this.unidad_medida = unidad_medida;
+	}
+
+	public List<Serie> getSerie() {
+		return serie;
+	}
+
+	public void setSerie(List<Serie> serie) {
+		this.serie = serie;
+	}
+
+	static class Serie {
+		private double valor;
+		private String fecha;
+
+		public double getValor() {
+			return valor;
+		}
+
+		public String getFecha() {
+			return fecha;
+		}
+
+	}
+}
--- src/main/java/com/devops/dxc/devops/model/Util.java
+++ src/main/java/com/devops/dxc/devops/model/Util.java
@@ -1,34 +1,68 @@
 package com.devops.dxc.devops.model;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.web.client.RestTemplate;
+
 public class Util {
 
-    /**
-     * Método para cacular el 10% del ahorro en la AFP.  Las reglas de negocio se pueden conocer en 
-     * https://www.previsionsocial.gob.cl/sps/preguntas-frecuentes-nuevo-retiro-seguro-10/
-     * 
-     * @param ahorro
-     * @param sueldo
-     * @return
-     */
-    public static int getDxc(int ahorro, int sueldo){
-        if(((ahorro*0.1)/getUf()) > 150 ){
-            return (int) (150*getUf()) ;
-        } else if((ahorro*0.1)<=1000000 && ahorro >=1000000){
-            return (int) 1000000;
-        } else if( ahorro <=1000000){
-            return (int) ahorro;
-        } else {
-            return (int) (ahorro*0.1);
-        }
-    }
+	private final static Logger LOGGER = Logger.getLogger("devops.subnivel.Modelo");
 
-    /**
-     * Método que retorna el valor de la UF.  Este método debe ser refactorizado por una integración a un servicio
-     * que retorne la UF en tiempo real.  Por ejemplo mindicador.cl
-     * @return
-     */
-    public static int getUf(){
-        return 29000;
-    }
-    
+	/**
+	 * Método para cacular el 10% del ahorro en la AFP. Las reglas de negocio se
+	 * pueden conocer en
+	 * https://www.previsionsocial.gob.cl/sps/preguntas-frecuentes-nuevo-retiro-seguro-10/
+	 * 
+	 * @param ahorro
+	 * @param sueldo
+	 * @return
+	 */
+	public static int getDxc(int ahorro, int sueldo) {
+
+		if (((ahorro * 0.1) / getUf()) > 150) {
+			return (int) (150 * getUf());
+		} else if ((ahorro * 0.1) <= 1000000 && ahorro >= 1000000) {
+			return (int) 1000000;
+		} else if (ahorro <= 1000000) {
+			return (int) ahorro;
+		} else {
+			return (int) (ahorro * 0.1);
+		}
+	}
+
+	/**
+	 * Método que retorna el valor de la UF. Este método debe ser refactorizado por
+	 * una integración a un servicio que retorne la UF en tiempo real. Por ejemplo
+	 * mindicador.cl
+	 * 
+	 * @return
+	 */
+	public static int getUf() {
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		Date date = new Date();
+
+		try {
+			Uf valor = restTemplate.getForObject((new StringBuilder()).append("https://mindicador.cl/api/uf/")
+					.append(dateFormat.format(date)).toString(), Uf.class);
+
+			LOGGER.log(Level.INFO, "Valor Uf: " + valor.getSerie().get(0).getValor());
+
+			return (int) valor.getSerie().get(0).getValor();
+
+		} catch (Exception e) {
+			return 29000;
+		}
+
+	}
+
 }
