73c6a3cee35c4e14a049387128f0ea283296c564
Jorge Parra <jorge.parra@karibu.cl>
2021-02-06 19:41:59 -0300

    Se grega test para instancias de Util Y App

--- src/test/java/com/devops/dxc/devops/DevopsApplicationTests.java
+++ src/test/java/com/devops/dxc/devops/DevopsApplicationTests.java
@@ -1,55 +1,52 @@
 package com.devops.dxc.devops;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
-import org.springframework.boot.test.context.SpringBootTest;
-import com.devops.dxc.devops.model.Util;
-import static org.junit.jupiter.api.Assertions.*;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import static java.time.Duration.ofSeconds;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import com.devops.dxc.devops.model.Util;
 
 @SpringBootTest
 class DevopsApplicationTests {
 
-	private static Util UTIL = null;
 	private final String CONSULTARUT = "8388316-2";
 	private final List<String> AFILIADOS = Arrays.asList("13706546-0", "13527931-5", "8388316-2");
 	private final List<String> DEUDORES = Arrays.asList("11013725-7", "13706546-0", "13527931-5", "8388316-2");
 
 	@Test
+	@DisplayName("Prueba de AppContext")
+	public void applicationContextTest() {
+		DevopsApplication.main(new String[] {});
+	}
+
+	@Test
 	@DisplayName("NO EXCEDE TIMEOUT CALCULO UF API")
 	public void whenAssertingTimeout_thenNotExceeded() {
 		assertTimeout(ofSeconds(6), () -> {
-			UTIL.getUf();
+			Util.getUf();
 		});
 	}
 
 	@Test
 	@DisplayName("¿TENGO SALDO?")
 	public void whenAssertingTengoSaldo() {
-		assertEquals(9000000, UTIL.getSaldo(10000000, 1000000));
+		assertEquals(9000000, Util.getSaldo(10000000, 1000000));
 	}
 
 	@Test
 	@DisplayName("¿TENGO QUE PAGAR IMPUESTOS?")
 	public void whenAssertingImpuestos() {
-		assertEquals(320000, UTIL.getImpuesto(2000000, 4000000));
+		assertEquals(320000, Util.getImpuesto(2000000, 4000000));
 	}
 
 	@Test
@@ -61,13 +58,13 @@ class DevopsApplicationTests {
 	@Test
 	@DisplayName("¿PUEDO RETIRAR TODOS MIS AHORROS?")
 	public void whenAssertingRetirarTodo() {
-		assertEquals(900000, UTIL.getDxc(900000, 600000));
+		assertEquals(900000, Util.getDxc(900000, 600000));
 	}
 
 	@Test
 	@DisplayName("¿PUEDO REALIZAR OTRO RETIRO DEL 10% ?")
 	public void whenAssertingRealizarOtroRetiro() {
-		assertEquals(900000, UTIL.getDxc(900000, 600000));
+		assertEquals(900000, Util.getDxc(900000, 600000));
 		assertThat(AFILIADOS, hasItems(CONSULTARUT));
 
 	}
@@ -75,7 +72,7 @@ class DevopsApplicationTests {
 	@Test
 	@DisplayName("MONTO MAXIMO DEL 10%")
 	public void whenAssertingMontoMaximo() {
-		assertTrue(UTIL.getDxc(100000000, 3000000) == (150 * UTIL.getUf()), "monto maximo permitido");
+		assertTrue(Util.getDxc(100000000, 3000000) == (150 * Util.getUf()), "monto maximo permitido");
 
 	}
 
--- src/test/java/com/devops/dxc/devops/model/UtilTest.java
+++ src/test/java/com/devops/dxc/devops/model/UtilTest.java
@@ -7,20 +7,17 @@ import org.junit.jupiter.api.Test;
 
 class UtilTest {
 
-	private Util util;
-
 	@BeforeEach
 	public void setUp() {
-
+		new Util();
 	}
 
 	@Test
 	void testImpuesto() {
-		util = new Util();
-		assertEquals(405000, util.getImpuesto(2500000, 3000000));
-		assertEquals(690000, util.getImpuesto(3500000, 3000000));
-		assertEquals(912000, util.getImpuesto(4500000, 3000000));
-		assertEquals(1050000, util.getImpuesto(6500000, 3000000));
+		assertEquals(405000, Util.getImpuesto(2500000, 3000000));
+		assertEquals(690000, Util.getImpuesto(3500000, 3000000));
+		assertEquals(912000, Util.getImpuesto(4500000, 3000000));
+		assertEquals(1050000, Util.getImpuesto(6500000, 3000000));
 	}
 
 }
