4c638dd18ab098e7dee8c6a16e2d055cd392b6a5
Jorge Parra <jorge.parra@karibu.cl>
2021-02-06 19:25:08 -0300

    Eliminar codigo de excepciones validadas en ValidationTest

--- src/test/java/com/devops/dxc/devops/rest/RestDataTest.java
+++ src/test/java/com/devops/dxc/devops/rest/RestDataTest.java
@@ -1,10 +1,8 @@
 package com.devops.dxc.devops.rest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 
 class RestDataTest {
 
@@ -12,20 +10,6 @@ class RestDataTest {
 	void testGetData() {
 		RestData restData = new RestData();
 		assertEquals(600000, restData.getData("600000", "2000000").getSueldo());
-	}
-
-	@Test
-	void testNumberFormatException() {
-		RestData restData = new RestData();
-
-		assertThrows(NumberFormatException.class, () -> {
-			restData.getData("ahorro", "2000000").getSueldo();
-		});
-
-		assertThrows(NumberFormatException.class, () -> {
-			restData.getData("2000000", "sueldo").getSueldo();
-		});
-
 	}
 
 }
